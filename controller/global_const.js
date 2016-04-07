7aa7f0a7736fa2b5541d2dd050503ac0fdf4fb5e
Your Name <you@example.com>
2016-04-07 11:10:26 +0300

    Constants changed

--- controller/global_const.js
+++ controller/global_const.js
@@ -1,8 +1,8 @@
-module.exports.ROWS = 200;
-module.exports.COLS = 200;
+module.exports.ROWS = 100;
+module.exports.COLS = 100;
 module.exports.CELL_SIZE = 10;
 module.exports.TIMEOUT = 1000;
-module.exports.GENERATION_LIMIT = 15;
+module.exports.GENERATION_LIMIT = 100;
 // set this to false if you're using Heroku or other hosting
 // localhost is true, when testing on your local machine
 module.exports.LOCALHOST = false;
